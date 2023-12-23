3594e34fced7b450976485fac9c9de418afea72a
backas36 <backas36@gmail.com>
2023-12-23 14:47:48 +0800

    chore: setup testing

---
+++ src/components/Hello.spec.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+
+import Hello from "./Hello";
+
+it("renders 'Hello'", () => {
+  render(<Hello />);
+  const myElement = screen.getByText(/Hello/);
+  expect(myElement).toBeInTheDocument();
+});
---
+++ vitest.config.ts
@@ -0,0 +1,30 @@
+import react from "@vitejs/plugin-react-swc";
+
+import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  plugins: [tsconfigPaths(), react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: path.resolve(__dirname, "./vitest.setup.ts"),
+    coverage: {
+      reporter: ["text", "json", "html"],
+      include: ["src/**/*"],
+      all: true,
+      //thresholds: {
+      //  lines: 100,
+      //  functions: 100,
+      //  branches: 100,
+      //  statements: 100,
+      //},
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
